7111c9a9bfcb5edfdc67669b01ab79e6a6d9e9fe
Dan Pappo <dpappo@gmail.com>
2021-02-15 14:30:04 -0500

    added random string editor, and post form method

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,6 +37,16 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substr(2, 6);
+};
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
