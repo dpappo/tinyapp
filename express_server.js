746f49e148e5d96ac942ad2d6ab43797bbc07c21
Dan Pappo <dpappo@gmail.com>
2021-02-15 14:09:38 -0500

    added view for new url and get method for new url

--- express_server.js
+++ express_server.js
@@ -17,6 +17,10 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
