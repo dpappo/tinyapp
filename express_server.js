b41785f725db3c305972801e5cc1a421d02d4827
Dan Pappo <dpappo@gmail.com>
2021-02-15 14:38:51 -0500

    implemented get with random generator, and localhost link on urls view

--- express_server.js
+++ express_server.js
@@ -39,8 +39,10 @@ app.get('/hello', (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  // console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortenedURL = generateRandomString();
+  urlDatabase[shortenedURL] = req.body.longURL;
 });
 
 const generateRandomString = function() {
