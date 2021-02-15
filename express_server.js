a0461df438b565a21af2425b7f92fbb5cff0064d
Dan Pappo <dpappo@gmail.com>
2021-02-15 14:58:18 -0500

    implemented redirect for new URLS

--- express_server.js
+++ express_server.js
@@ -40,9 +40,10 @@ app.get('/hello', (req, res) => {
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
   let shortenedURL = generateRandomString();
   urlDatabase[shortenedURL] = req.body.longURL;
+  res.redirect('/urls/' + shortenedURL);
 });
 
 const generateRandomString = function() {
