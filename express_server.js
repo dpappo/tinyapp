57260db6f7789f97247ef62ebf34b9f1ecd0e018
Dan Pappo <dpappo@gmail.com>
2021-02-15 15:33:56 -0500

    implemented delete functionality

--- express_server.js
+++ express_server.js
@@ -46,6 +46,12 @@ app.post("/urls", (req, res) => {
   res.redirect('/urls/' + shortenedURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
