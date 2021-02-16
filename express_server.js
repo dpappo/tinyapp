127ae49fcd1d79277e717488ede8d09ff763c0e8
Dan Pappo <dpappo@gmail.com>
2021-02-16 09:13:33 -0500

    implemented userID + registration in a database aka object

--- express_server.js
+++ express_server.js
@@ -14,6 +14,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 app.get('/', (req, res) => {
   const templateVars = {urls: urlDatabase,
     username: req.cookies["username"],
@@ -72,6 +85,17 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
+  res.redirect(`/urls/`);
+});
+
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie('userID', userID);
   res.redirect(`/urls/`);
 });
 
